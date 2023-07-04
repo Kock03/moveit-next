e72a1226d91de3909e4ce41313bfd24d96b4a784
Kock03 <vinikk03@gmail.com>
2023-07-04 17:37:18 -0300

    second class

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
+import { Profile } from "@/components/Profile";
 import ExperienceBar from "../components/ExperienceBar";
+import styles from "../styles/pages/Home.module.css";
+import { CompleteChallenges } from "@/components/CompleteChallenges";
+import { Countdown } from "@/components/Countdown";
+
+import Head from "next/head";
 
 export default function Home() {
   return (
-    <div className="container">
+    <div className={styles.container}>
+      <Head>
+        <title>Inicio | move.it</title>
+      </Head>
       <ExperienceBar />
-
       <section>
-        
+        <div>
+          <Profile />
+          <CompleteChallenges />
+          <Countdown />
+        </div>
       </section>
     </div>
   )
